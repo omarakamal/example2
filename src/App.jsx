dab20780e7cc8fb4b415c4a71ceb565eb9b231c7
Omar <omaraakamal@gmail.com>
2024-02-13 16:05:09 +0400

    Basic Routing complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
 import './App.css'
+import {Routes,Route} from 'react-router-dom'
+import StudentDetails from './pages/StudentDetails'
+import Students from './pages/Students'
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route path='/students/:id' element={<StudentDetails/>}></Route>
+        <Route path='/students' element={<Students/>}></Route>
+      </Routes>
     </>
   )
 }
