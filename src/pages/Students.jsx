3a9bb321d11f529ae22f72de9820a382c2d4159d
Omar <omaraakamal@gmail.com>
2024-02-13 16:01:36 +0400

    Axios call with mapping basic layout

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {useEffect,useState} from 'react'
+import axios from 'axios'
 
 function Students() {
+    const [students,setStudents] = useState([])
+    axios.get('https://omar-class-api.adaptable.app/students')
+    .then((res)=>{
+        setStudents(res.data)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
   return (
-    <div>Students</div>
+    <div>
+        {students.map((student)=>{
+            return(
+                <div key={student.id}>
+                    <h1>{student.name}</h1>
+                </div>
+            )
+        })}
+    </div>
   )
 }
 
